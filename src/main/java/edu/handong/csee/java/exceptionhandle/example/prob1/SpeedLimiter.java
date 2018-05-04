e20355830e3ab52dc8247581c43ce5f76eb6dc34
21700500Myeonghui <21700500@handong.edu>
2018-05-04 23:19:29 +0900

    prob1

---
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/SpeedLimiter.java
@@ -0,0 +1,34 @@
+package edu.handong.csee.java.exceptionhandle.example.prob1;//Instructs the SpeedLimiter class to be complied and stored in edu.handong.csee.java.exceptionhandle.example.prob1
+
+public class SpeedLimiter//The declaration of class SpeedLimiter
+{
+
+	private int speedLimit;//Create integer type Instance variable speedLimit
+	private int currentSpeed;//Create integer type Instance variable currentSpeed
+
+
+	public SpeedLimiter(int speedLimit, int currentSpeed)//Constructor with parameter, parameters are integer type speedLimit and integer type currentSpeed
+	{
+		this.speedLimit=speedLimit;//put the parameter speedLimit into the member variable speedLimit, 'this' is a reference to the object itself
+		this.currentSpeed=currentSpeed;//put the parameter currentSpeed into the member variable currentSpeed, 'this' is a reference to the object itself
+	}
+
+	public void warnSpeedLimit()//The declaration of method warnSpeedLimit() without return value
+	{
+		try//try block contains code where something could possibly go wrong
+
+		{
+			if(this.speedLimit<this.currentSpeed)//If member variable speedLimit is less than member variable currentSpeed. 'this' is a reference to the object itself
+				throw new Exception("Speed Limit "+this.speedLimit+"km exceeded!");//Create an object of the Exception class, the parameter is "Speed Limit 'value of variable speedLimit'km exceeded!", throw Exception exception
+			System.out.println("You are a law abiding citizen!");//Outputs the string literal "You are a law abiding citizen!"
+
+		}
+		catch(Exception e)//Exception handling code, Exception information is passed to object e.
+		{
+			System.out.println(e.getMessage());//calling method getMessage() in e, Output return value of method getMessage() in e
+			System.out.println("You are being fined.");//Outputs the string literal "You are fined." 
+		}
+		System.out.println("Your current speed: "+this.currentSpeed);//Output "Your current speed 'value of variable currentSpeed'", 'this' is a reference to the object itself
+	}
+
+}
---
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/SpeedLimiter_main.java
@@ -0,0 +1,31 @@
+package edu.handong.csee.java.exceptionhandle.example.prob1;//Instructs the SpeedLimiter_main class to be complied and stored in edu.handong.csee.java.exceptionhandle.example.prob1
+
+import java.util.Scanner;//importing a package, java.util.Scanner. A package is a kind of external objects created by other developer
+
+import java.util.Random;//importing a package, java.util.Random. A package is a kind of external objects created by other developer
+
+public class SpeedLimiter_main//The declaration of class SpeedLimiter_main
+{
+
+	public static void main(String[] args)//The declaration of static main method without return value
+	{
+		// TODO Auto-generated method stub
+
+		Scanner keyboard= new Scanner(System.in);//Create an object of the Scanner class
+		System.out.print("Set the speed limit, officer: ");//Outputs the string literal "Set the speed limit, officer: "
+
+		int speedLimit=keyboard.nextInt();//Create integer type local variable speedLimit, Take one inteer from the keyboard and place it in the speedLimit
+
+		Random randomGen = new Random();//Create an object of the Random class
+
+		int currentSpeed = randomGen.nextInt(100);//Create integer type local variable currentSpeed, Create random number from 0 to 10, put it in the currentSpeed
+
+		SpeedLimiter spl = new SpeedLimiter(speedLimit, currentSpeed);//Instantiate of an SpeedLimiter object, calling constructor, value of parameters are value of variable speedLimit and value of variable currentSpeed at this time
+		spl.warnSpeedLimit();//calling method warnSpeedLimit() in spl
+
+		keyboard.close();//close object Scanner
+
+
+	}
+
+}
